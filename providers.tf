857b9fa9a4e9cf4ecc190312983e410731107680
nagarajumaddila <94115536+nagarajumaddila@users.noreply.github.com>
2021-11-12 15:00:08 +0530

    Add files via upload

--- providers.tf
+++ providers.tf
@@ -1,7 +1,25 @@
-provider "google" {
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "nagaraju4238-training"
+
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "hashicat-gcp"
+    }
+  }
+}
+/*provider "google" {
   project = var.project_id
   region  = var.region
-}
+}*/
 
 /*terraform {
   backend "gcs" {
@@ -10,12 +28,12 @@ provider "google" {
   }
 }*/
 
-terraform {
+/*terraform {
   backend "gcs" {
     project = var.project_id
     bucket  = "terraform-state"
     prefix  = "terraform/state/dev/d001"
-    credentials = "${var.credentials}"
+    credentials = "credentials.json"
   }
 }
 
@@ -27,4 +45,4 @@ data "terraform_remote_state" "d001" {
     prefix  = "terraform/state/dev/d001"
     credentials = "${var.credentials}"
   }
-}
+}*/
