c1e54f93c550d2e32e9452b0b62dc6722fb60256
nagarajumaddila <94115536+nagarajumaddila@users.noreply.github.com>
2021-11-12 14:35:35 +0530

    Add files via upload

--- providers.tf
+++ providers.tf
@@ -3,9 +3,28 @@ provider "google" {
   region  = var.region
 }
 
-terraform {
+/*terraform {
   backend "gcs" {
     bucket = "gcp-terraform-github-actions-test"
     prefix = "terraform/state"
+  }
+}*/
+
+terraform {
+  backend "gcs" {
+    project = var.project_id
+    bucket  = "terraform-state"
+    prefix  = "terraform/state/dev/d001"
+    credentials = "credentials.json"
+  }
+}
+
+data "terraform_remote_state" "d001" {
+  backend = "gcs"
+  config {
+    project = var.project_id
+    bucket  = "terraform-state"
+    prefix  = "terraform/state/dev/d001"
+    credentials = "${var.credentials}"
   }
 }
